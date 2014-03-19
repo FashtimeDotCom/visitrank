bb58f7ed4220b37d35752140d0edeb4ec780d117
jianglibo <jianglibo@gmail.com>
2014-03-19 10:56:34 +0800

    appender now work

---
+++ src/main/java/com/m3958/visitrank/logger/UrlPersistorLogger.java
@@ -0,0 +1,20 @@
+package com.m3958.visitrank.logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.vertx.java.core.json.JsonObject;
+
+public class UrlPersistorLogger {
+
+  static final Logger urlPersistor = LogManager.getLogger("UrlPersistor");
+
+
+  // Set up a simple configuration that logs on the console.
+  public static void main(String[] args) {
+    JsonObject jo = new JsonObject().putString("abc", "hello world").putNumber("number", 55);
+    urlPersistor.trace("Entering application.");
+    urlPersistor.warn(jo);
+    urlPersistor.trace("Exiting application.");
+  }
+
+}
